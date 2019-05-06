3b8bf63a127674b2849e66d639bfa7a022555c97
krikaz <pascal.bou@protonmail.com>
2019-05-06 18:23:34 +0200

    function-conversion.js, solved all challenges

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -14,11 +14,15 @@
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 
 // Stretch
@@ -28,3 +32,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => num*3);
+console.log(triple);
